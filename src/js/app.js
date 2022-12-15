32e95ce91222b20aecf1d588ea53b74bed398001
ivaylo91 <ipenev91@gmail.com>
2022-12-15 14:04:34 +0200

    set attributte to every product div

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,14 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-
+    // This block will be executed once the page is loaded and ready
+    let productDiv = document.querySelector('.product');
+    let productPrice = document.querySelectorAll('p')[1].innerHTML;
+    let span = document.createElement('span');
+    span.class = 'data-price';
+    span.className = 'data-price';
+    span.innerHTML = 'product-price';
+    productDiv.setAttribute('data-price', productPrice);
+    productDiv.appendChild(span);
 
 });
